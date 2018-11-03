fb081aaeeb54f0a3a25dc322b81f95e2912477cf
alvinVMWare <alvinchristianto93@gmail.com>
2018-11-04 01:43:29 +0700

    seventh commit

--- go.py
+++ go.py
@@ -1,11 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-def go_live() :
+def go_live():
+    headerText = []
     url = "https://www.gsmarena.com/apple_iphone_xs-9318.php"
     content = urllib2.urlopen(url).read()
     soup = BeautifulSoup(content, "html.parser")
     
     headerDiv = soup.find("div", {"class": "article-info-line page-specs light border-bottom"})
     headerText = headerDiv.find("h1").getText()
-    print headerText
+   # print headerText
+    return headerText
+if __name__ == "__main__":
+    print(go_live())
+    
+
--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from flask import Flask
-#from go import go_live
+from go import go_live
 
 first_app = Flask(__name__)
 
 @first_app.route('/')
 
 def hello_world():
-    return 'Hello world'
-    #return scrape()
+ #   return 'Hello world'
+    return go_live()
 if __name__ == '__main__' :
     first_app.run()
