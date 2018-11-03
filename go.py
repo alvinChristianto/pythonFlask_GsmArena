e10a57c89b6c405baa5a9fd2014d138d2a475db8
alvinVMWare <alvinchristianto93@gmail.com>
2018-11-04 01:59:38 +0700

    eight commit

--- go.py
+++ go.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup
-import urllib2
+import requests
 
 def go_live():
     headerText = []
     url = "https://www.gsmarena.com/apple_iphone_xs-9318.php"
-    content = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(content, "html.parser")
+    content = requests.get(url)
+   # content = urllib.urlopen(url).read()
+    soup = BeautifulSoup(content.text, "html.parser")
     
     headerDiv = soup.find("div", {"class": "article-info-line page-specs light border-bottom"})
     headerText = headerDiv.find("h1").getText()
